99f37c997f86332552a9368a8a8e10c5a69fa0dc
Visibyte <nathanglass26@gmail.com>
2023-11-12 19:32:46 +0000

    allow the ability to add X to any square

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,5 +1,14 @@
-const Square = () => {
-  return <button className="square">X</button>;
+type Value = {
+  value: string | null;
+  onSquareClick: () => void;
+};
+
+const Square = ({ value, onSquareClick }: Value) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
